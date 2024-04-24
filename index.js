39cbf5f3fd409898dbdf05af879123c710215f70
Muxammadamin Zoirov <muxammadaminzoirov@gmail.com>
2024-04-24 10:17:14 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,23 @@
+let son1 = Number(prompt("Birinchi sonni kriting"));
+let son2 = Number(prompt("Icinchi sonni kriting"));
+let amal = String(prompt("Amalni kiriting (+, -, *, /): "));
+
+if (amal === "+"); {
+    let yigindi = son1 + son2;
+    alert(son1 + " + " + son2 + " = " + yigindi);
+}
+
+if (amal === "-"); {
+    let yigindi = son1 - son2;
+    alert(son1 + " - " + son2 + " = " + yigindi);
+}
+
+if (amal === "*"); {
+    let yigindi = son1 * son2;
+    alert(son1 + " * " + son2 + " = " + yigindi);
+}
+
+if (amal === "/"); {
+    let yigindi = son1 + son2;
+    alert(son1 + " / " + son2 + " = " + yigindi);
+}
